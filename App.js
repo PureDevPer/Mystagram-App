d35d4be1890dd98cd00ac6b55600c69eaeb80a76
PureDevPer <wooseok.kim1207@gmail.com>
2019-08-27 11:13:24 -0400

    AuthContext

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Ionicons } from '@expo/vector-icons';
 import { AppLoading } from 'expo';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
-import { Text, View, AsyncStorage, TouchableOpacity } from 'react-native';
+import { AsyncStorage } from 'react-native';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { persistCache } from 'apollo-cache-persist';
 import ApolloClient from 'apollo-boost';
@@ -11,11 +11,13 @@ import { ApolloProvider } from 'react-apollo-hooks';
 import apolloClientOptions from './apollo';
 import { ThemeProvider } from 'styled-components';
 import styles from './styles';
+import NavController from './components/NavController';
+import { AuthProvider } from './AuthContext';
 
 export default function App() {
 	const [loaded, setLoaded] = useState(false);
 	const [client, setClient] = useState(null);
-	const [isLoggedIn, setIsLoggedIn] = useState(null);
+
 	const preLoad = async () => {
 		try {
 			await Font.loadAsync({
@@ -33,13 +35,6 @@ export default function App() {
 				...apolloClientOptions
 			});
 
-			const isLoggedIn = await AsyncStorage.getItem('isLoggedIn');
-			if (isLoggedIn === null || isLoggedIn === 'false') {
-				setIsLoggedIn(false);
-			} else {
-				setIsLoggedIn(true);
-			}
-
 			// If everything is loaded, setLoaded is true and setClient is client
 			setLoaded(true);
 			setClient(client);
@@ -51,42 +46,14 @@ export default function App() {
 		preLoad();
 	}, []);
 
-	const logUserIn = async () => {
-		try {
-			await AsyncStorage.setItem('isLoggedIn', 'true');
-			setIsLoggedIn(true);
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	const logUserOut = async () => {
-		try {
-			await AsyncStorage.setItem('isLoggedIn', 'false');
-			setIsLoggedIn(false);
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
 	// Once component is mounted, loaded is false and client is null
 	// Then, go to AppLoading
-	return loaded && client && isLoggedIn !== null ? (
+	return loaded && client ? (
 		<ApolloProvider client={client}>
 			<ThemeProvider theme={styles}>
-				<View
-					style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-				>
-					{isLoggedIn === true ? (
-						<TouchableOpacity onPress={logUserOut}>
-							<Text>Log Out</Text>
-						</TouchableOpacity>
-					) : (
-						<TouchableOpacity onPress={logUserIn}>
-							<Text>Log In</Text>
-						</TouchableOpacity>
-					)}
-				</View>
+				<AuthProvider>
+					<NavController />
+				</AuthProvider>
 			</ThemeProvider>
 		</ApolloProvider>
 	) : (
---
+++ AuthContext.js
@@ -0,0 +1,46 @@
+import React, { createContext, useContext, useState } from 'react';
+import { AsyncStorage } from 'react-native';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+	const [isLoggedIn, setIsLoggedIn] = useState(null);
+	const logUserIn = async () => {
+		try {
+			await AsyncStorage.setItem('isLoggedIn', 'true');
+			setIsLoggedIn(true);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const logUserOut = async () => {
+		try {
+			await AsyncStorage.setItem('isLoggedIn', 'false');
+			setIsLoggedIn(false);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<AuthContext.Provider value={{ isLoggedIn, logUserIn, logUserOut }}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+export const useIsLoggedIn = () => {
+	const { isLoggedIn } = useContext(AuthContext);
+	return isLoggedIn;
+};
+
+export const useLogIn = () => {
+	const { logUserIn } = useContext(AuthContext);
+	return;
+};
+
+export const useLogOut = () => {
+	const { logUserOut } = useContext(AuthContext);
+	return;
+};
