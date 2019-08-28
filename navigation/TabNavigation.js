d2b15b595963a35cc25c23d4d6355d02c2a3552d
PureDevPer <wooseok.kim1207@gmail.com>
2019-08-28 13:04:26 -0400

    Message Navigation

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,19 +1,49 @@
+import React from 'react';
+import {
+	createBottomTabNavigator,
+	createStackNavigator
+} from 'react-navigation';
+import Home from '../screens/Tabs/Home';
+import Search from '../screens/Tabs/Search';
+import Notifications from '../screens/Tabs/Notifications';
+import Profile from '../screens/Tabs/Profile';
+import MessagesLink from '../components/MessagesLink';
 import { View } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation';
-import Home from '../screens/Home';
-import Search from '../screens/Search';
-import Notifications from '../screens/Notifications';
-import Profile from '../screens/Profile';
+
+const stackFactory = (initialRoute, customConfig) =>
+	createStackNavigator({
+		InitialRoute: {
+			screen: initialRoute,
+			navigationOptions: { ...customConfig }
+		}
+	});
 
 export default createBottomTabNavigator({
-	Home,
-	Search,
+	Home: {
+		screen: stackFactory(Home, {
+			title: 'Home',
+			headerRight: <MessagesLink />
+		})
+	},
+	Search: {
+		screen: stackFactory(Search, {
+			title: 'Search'
+		})
+	},
 	Add: {
 		screen: View,
 		navigationOptions: {
 			tabBarOnPress: ({ navigation }) => navigation.navigate('PhotoNavigation')
 		}
 	},
-	Notifications,
-	Profile
+	Notifications: {
+		screen: stackFactory(Notifications, {
+			title: 'Notifications'
+		})
+	},
+	Profile: {
+		screen: stackFactory(Profile, {
+			title: 'Profile'
+		})
+	}
 });
