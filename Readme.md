ec6f308ba26873b023f7a944d2a7aeeac8c5901f
PureDevPer <wooseok.kim1207@gmail.com>
2019-08-28 17:01:38 -0400

    Login

--- Readme.md
+++ Readme.md
@@ -18,3 +18,4 @@ Screens:
 - Expo Vector Icon: https://github.com/expo/vector-icons
 - Apollo Cache Persist: https://github.com/apollographql/apollo-cache-persist
 - React Navigation: https://reactnavigation.org/docs/en/stack-navigator.html
+- TextInput: https://facebook.github.io/react-native/docs/textinput
--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import AuthButton from '../../components/AuthButton';
 import AuthInput from '../../components/AuthInput';
 import useInput from '../../hooks/useInput';
+import { Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 const View = styled.View`
 	justify-content: center;
@@ -14,14 +15,31 @@ const Text = styled.Text``;
 
 export default () => {
 	const emailInput = useInput('');
+	const handleLogin = () => {
+		const { value } = emailInput;
+		const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+		if (value === '') {
+			return Alert.alert("Email can't be empty");
+		} else if (!value.includes('@') || !value.includes('.')) {
+			return Alert.alert('Please write an email');
+		} else if (!emailRegex.test(value)) {
+			return Alert.alert('That email is invalid');
+		}
+	};
 	return (
-		<View>
-			<AuthInput
-				{...emailInput}
-				placeholder="Email"
-				keyboardType="email-address"
-			/>
-			<AuthButton text="Log In" onPress={() => null} />
-		</View>
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+			<View>
+				<AuthInput
+					{...emailInput}
+					placeholder="Email"
+					keyboardType="email-address"
+					returnKeyType="send"
+					onEndEditing={handleLogin}
+					autoCorrect={false}
+				/>
+				<AuthButton text="Log In" onPress={handleLogin} />
+			</View>
+		</TouchableWithoutFeedback>
 	);
 };
