84cf33e16071c796909bf01a5802f5ed5c9d42ce
PureDevPer <wooseok.kim1207@gmail.com>
2019-08-29 13:44:15 -0400

    Component - Post, Added more data

--- components/Post.js
+++ components/Post.js
@@ -29,15 +29,35 @@ const Location = styled.Text`
 `;
 
 const IconsContainer = styled.View`
-	padding: 10px;
 	flex-direction: row;
+	margin-bottom: 5px;
 `;
 
 const IconContainer = styled.View`
 	margin-right: 10px;
 `;
 
-const Post = ({ user, location, files = [] }) => {
+const InfoContainer = styled.View`
+	padding: 10px;
+`;
+
+const Caption = styled.Text`
+	margin: 3px 0px;
+`;
+
+const CommentCount = styled.Text`
+	opacity: 0.5;
+	font-size: 12px;
+`;
+
+const Post = ({
+	user,
+	location,
+	files = [],
+	likeCount,
+	caption,
+	comments = []
+}) => {
 	return (
 		<Container>
 			<Header>
@@ -55,7 +75,8 @@ const Post = ({ user, location, files = [] }) => {
 				</Touchable>
 			</Header>
 			<Swiper
-				showsPagination={false}
+				showsPagination={true}
+				paginationStyle={{ position: 'absolute', bottom: -30 }}
 				style={{ height: constants.height / 2.5 }}
 			>
 				{files.map(file => (
@@ -66,28 +87,37 @@ const Post = ({ user, location, files = [] }) => {
 					/>
 				))}
 			</Swiper>
-			<IconsContainer>
+			<InfoContainer>
+				<IconsContainer>
+					<Touchable>
+						<IconContainer>
+							<Ionicons
+								size={28}
+								name={
+									Platform.OS === 'ios' ? 'ios-heart-empty' : 'md-heart-empty'
+								}
+							/>
+						</IconContainer>
+					</Touchable>
+					<Touchable>
+						<IconContainer>
+							<Ionicons
+								size={28}
+								name={Platform.OS === 'ios' ? 'ios-text' : 'md-text'}
+							/>
+						</IconContainer>
+					</Touchable>
+				</IconsContainer>
 				<Touchable>
-					<IconContainer>
-						<Ionicons
-							size={28}
-							name={
-								Platform.OS === 'ios' ? 'ios-heart-empty' : 'md-heart-empty'
-							}
-						/>
-					</IconContainer>
+					<Bold>{likeCount === 1 ? '1 like' : `${likeCount} likes`}</Bold>
 				</Touchable>
+				<Caption>
+					<Bold>{user.username}</Bold> {caption}
+				</Caption>
 				<Touchable>
-					<IconContainer>
-						<Ionicons
-							size={28}
-							name={
-								Platform.OS === 'ios' ? 'ios-heart-empty' : 'md-heart-empty'
-							}
-						/>
-					</IconContainer>
+					<CommentCount>See all {comments.length} comments</CommentCount>
 				</Touchable>
-			</IconsContainer>
+			</InfoContainer>
 		</Container>
 	);
 };
