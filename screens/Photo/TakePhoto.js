358e710be349dd6c91d86d24b9fcb862720e5431
PureDevPer <wooseok.kim1207@gmail.com>
2019-08-30 16:10:06 -0400

    Take Photo - Added icon

--- screens/Photo/TakePhoto.js
+++ screens/Photo/TakePhoto.js
@@ -4,14 +4,19 @@ import { Camera } from 'expo-camera';
 import * as Permissions from 'expo-permissions';
 import constants from '../../constants';
 import Loader from '../../components/Loader';
+import { TouchableOpacity, Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const View = styled.View`
 	flex: 1;
 `;
 
+const Icon = styled.View``;
+
 export default ({ navigation }) => {
 	const [loading, setLoading] = useState(true);
 	const [hasPermission, setHasPermission] = useState(false);
+	const [cameraType, setCameraType] = useState(Camera.Constants.Type.front);
 	const askPermission = async () => {
 		try {
 			const { status } = await Permissions.askAsync(Permissions.CAMERA);
@@ -26,6 +31,13 @@ export default ({ navigation }) => {
 			setLoading(false);
 		}
 	};
+	const toggleType = () => {
+		if (cameraType === Camera.Constants.Type.front) {
+			setCameraType(Camera.Constants.Type.back);
+		} else {
+			setCameraType(Camera.Constants.Type.front);
+		}
+	};
 	useEffect(() => {
 		askPermission();
 	}, []);
@@ -35,8 +47,28 @@ export default ({ navigation }) => {
 				<Loader />
 			) : hasPermission ? (
 				<Camera
-					style={{ width: constants.width, height: constants.height / 2 }}
-				/>
+					type={cameraType}
+					style={{
+						justifyContent: 'flex-end',
+						padding: 15,
+						width: constants.width,
+						height: constants.height / 2
+					}}
+				>
+					<TouchableOpacity onPress={toggleType}>
+						<Icon>
+							<Ionicons
+								name={
+									Platform.OS === 'ios'
+										? 'ios-reverse-camera'
+										: 'md-reverse-camera'
+								}
+								size={32}
+								color={'white'}
+							/>
+						</Icon>
+					</TouchableOpacity>
+				</Camera>
 			) : null}
 		</View>
 	);
